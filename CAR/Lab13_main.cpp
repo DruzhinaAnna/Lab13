3b1f58f688026c06ff1df1eb4235a823a3d0673a
DruzhinaAnna <91753354+DruzhinaAnna@users.noreply.github.com>
2021-10-04 21:41:35 +0300

    Add files via upload

---
+++ CAR/Lab13_main.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+#include "Car.h" 
+#include "Lorry.h" 
+using namespace std; 
+void f1(Car& c)
+{
+	c.Setmark("Opel"); cout << c;
+}
+Car f2() 
+{
+	Lorry l("Kia", 1, 2, 3);
+	return l;
+}
+int main()
+{ 
+	Car a; 
+	cin>>a;
+	cout<<a;
+	Car b("Ford",4,115); 
+	cout<<b;
+	a=b; 
+	cout<<a; 
+	a = f2();
+	cout << a;
+	Lorry c; 
+	cin >> c; 
+	cout << c;
+	system("PAUSE");
+}
